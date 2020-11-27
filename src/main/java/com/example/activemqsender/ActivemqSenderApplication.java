00978954973e0b7bbb0377e7b955c0e2f0dc1f59
Elena <gr.elena@mail.ru>
2020-11-27 16:24:53 +0500

    fix

--- src/main/java/com/example/activemqsender/ActivemqSenderApplication.java
+++ src/main/java/com/example/activemqsender/ActivemqSenderApplication.java
@@ -2,8 +2,15 @@ package com.example.activemqsender;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @SpringBootApplication
+@EnableAsync
 public class ActivemqSenderApplication {
 
     public static void main(String[] args) {
